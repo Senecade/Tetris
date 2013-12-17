809952f063865a47e7dd0b8433d208029aedfa52
VECcom <florian.thie@hotmail.com>
2013-12-17 14:14:49 +0100

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <pthread.h>
+#include <time.h>
 
 #define BLOCKS_PER_LEVEL 5
 
@@ -10,10 +11,13 @@
 #define LEFT_INT 3
 #define FALL_INT 4
 
-#define RIGHT go(RIGHT_INT);
-#define DOWN go(DOWN_INT);
-#define LEFT go(LEFT_INT);
-#define FALL go(FALL_INT);
+#define X 0
+#define Y 1
+
+#define RIGHT move(RIGHT_INT);
+#define DOWN move(DOWN_INT);
+#define LEFT move(LEFT_INT);
+#define FALL move(FALL_INT);
 
 #define TURN_RIGHT turn(RIGHT);
 #define TURN_LEFT turn(LEFT);
@@ -21,9 +25,9 @@
 int field[100][200] = {0}, block_num = 0, level = 0, delay = 1000;
 char running = 1;
 
-int go(int d){
-    switch(d){
-        case RIGHT_INT:
+int move(int direct){
+    switch(direct){
+        case RIGHT_INT: next
                         break;
         case DOWN_INT:
                         break;
@@ -50,8 +54,20 @@ void next_level(){
     delay = (int) (1000.0f / sqrt(level));
 }
 
+int shuffle(int start, int stop){
+    if(start >= stop)
+        return 0;
+    srand(time(0));
+    return (rand() % (stop - start + 1)) + start;
+}
+
+void transform_block(){
+    
+}
+
 void create_block(){
     block_num++;
+    
 }
 
 void spawn_block(){
@@ -59,6 +75,8 @@ void spawn_block(){
 }
 
 void next_block(){
+    transform_block();
+
     if(!(block_num % BLOCKS_PER_LEVEL))
         next_level();
     

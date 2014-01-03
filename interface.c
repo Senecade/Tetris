43cd5202cc4c7ec75545dad37b95e91b7a8c4767
Manuel Thieme <morion@morion-laptop>
2014-01-03 17:23:31 +0100

    added first two lines

--- interface.c
+++ interface.c
@@ -1,20 +1,23 @@
 #include <GL/glut.h>
-
+enum{
+    WINDOW_HEIGHT = 600,
+    WINDOW_WIDTH = 300
+};
 void drawBlock(int x, int y){
     float ox1,oy1,ox2,oy2,ix1,iy1,ix2,iy2;
     //Outline
     ox1 = -1 + x/5.0;
-    oy1 = 1 - y/10.0;
+    oy1 = 1 - (y-2)/10.0 ;
     ox2 = ox1 + 1/5.0;
     oy2 = oy1 - 1/10.0;
     
     glColor3f(0,0,0);
     glRectf(ox1,oy1,ox2,oy2);
     
-    ix1 = ox1 + 2/300.0;
-    iy1 = oy1 - 2/600.0;
-    ix2 = ox2 - 2/300.0;
-    iy2 = oy2 + 2/600.0;
+    ix1 = ox1 + 2.0/WINDOW_WIDTH;
+    iy1 = oy1 - 2.0/WINDOW_HEIGHT;
+    ix2 = ox2 - 2.0/WINDOW_WIDTH;
+    iy2 = oy2 + 2.0/WINDOW_HEIGHT;
     
     glColor3f(0,1,1);
     glRectf(ix1,iy1,ix2,iy2);
@@ -23,11 +26,18 @@ void drawBlock(int x, int y){
 void updateWindow(){
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
     glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer (background)
-    drawBlock(0,0);
-    drawBlock(0,1);
-    drawBlock(0,2);
-    drawBlock(1,1);
+    drawBlock(5,0);
+    drawBlock(5,1);
+    drawBlock(5,2);
+    drawBlock(6,1);
     glFlush();  // Render now
 
 }
-
+void initWindow(int argc, char** argv){
+    glutInit(&argc, argv);
+    glutInitWindowSize(WINDOW_WIDTH,WINDOW_HEIGHT);
+    glutInitWindowPosition(600,200);
+    glutCreateWindow("Tetris");
+    glutDisplayFunc(updateWindow);
+    glutMainLoop();
+}
--- main.c
+++ main.c
@@ -1,19 +1,12 @@
-#include <GL/glut.h>
 #include "interface.h"
+/*
 #include "functions.h"
 #include "move.h"
+*/
 #include <stdio.h>
 
-enum{
-    WINDOW_HEIGHT = 600,
-    WINDOW_WIDTH = 300
-};
+
 int main(int argc, char** argv) {
-    glutInit(&argc, argv);
-    glutInitWindowSize(WINDOW_WIDTH,WINDOW_HEIGHT);
-    glutInitWindowPosition(600,200);
-    glutCreateWindow("Tetris");
-    glutDisplayFunc(updateWindow);
-    glutMainLoop();
+    initWindow(argc, argv);
     return 0;
 }

bfbb75eab15e5ee45ddf57f04c7d20a44090805a
nubTaroop <nubtaroop@googlemail.com>
2014-01-05 15:58:27 +0100

    Lazy Interfaceprogrammers

--- globalshit.h
+++ globalshit.h
@@ -18,7 +18,7 @@
 #define RESULT_INT 1
 
 #define MOVE_BLOCK_RIGHT ActiveBlox.x++
-#define MOVE_BLOCK_DOWN do FALL while (possible(FALL_INT))
+#define MOVE_BLOCK_DOWN do FALL; while (possible(FALL_INT))
 #define MOVE_BLOCK_LEFT ActiveBlox.x--
 #define MOVE_BLOCK_FALL ActiveBlox.y++
 #define ROTATE_BLOCK_RIGHT rotate(ROTATION_RIGHT_INT)
